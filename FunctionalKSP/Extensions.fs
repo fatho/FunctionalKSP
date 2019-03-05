a0a73750496a288fe4a8ffe6fa7f45e5ffd6dfe8
Fabian Thorand <f.thorand@gmail.com>
2019-03-05 20:26:56 +0100

    Remove outdated project

--- FunctionalKSP/Extensions.fs
+++
@@ -1,8 +0,0 @@
-﻿module FunctionalKSP.Extensions
-
-open KRPC.Client
-
-type RemoteObject with
-    member obj.Connection with get(): Connection = downcast obj.connection
-
-let clamp<[<Measure>] 'u> (lo: float<'u>) (hi: float<'u>) (v: float<'u>): float<'u> = min hi (max lo v)
